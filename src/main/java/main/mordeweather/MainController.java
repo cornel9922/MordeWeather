5b1babdc7c1309c5ddd69c95e8ae38c258661d66
Cornel <cornel.cornel200@gmail.com>
2022-12-24 10:27:20 +0200

    Release! Fully working product

--- src/main/java/main/mordeweather/MainController.java
+++ src/main/java/main/mordeweather/MainController.java
@@ -12,6 +12,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
+
 public class MainController {
 
     @FXML
@@ -48,8 +49,11 @@ public class MainController {
             String output = getUrlContent("https://api.openweathermap.org/data/2.5/weather?q="+ getUserCity +"&appid=455b088558484b99c1e8bb09d1569fa4&units=metric");
             System.out.println(output);
             if (!output.isEmpty()) {
-
-
+                temperature_text.setText("Temperature: " + output.substring(output.indexOf("temp") + 6, output.indexOf("temp") + 9) + "°");
+                feels_like_text.setText("Feels like: " + output.substring(output.indexOf("feels_like") + 12, output.indexOf("feels_like") + 16) + "°");
+                minimum_text.setText("Minimum: " + output.substring(output.indexOf("temp_min") + 10, output.indexOf("temp_min") + 14) + "°");
+                maximum_text.setText("Maximum: " + output.substring(output.indexOf("temp_max") + 10, output.indexOf("temp_max") + 14) + "°");
+                pressure_text.setText("Pressure: " + output.substring(output.indexOf("pressure") + 10, output.indexOf("pressure") + 14) + " kPa");
             }
         });
     }
